d4e857a0fddfe2a60c3e037eb949402f8ab14957
hpx <m15257990085@163.com>
2019-07-28 20:17:46 +0800

    change db and redis address

--- session/conn.go
+++ session/conn.go
@@ -6,23 +6,9 @@ import (
 
 var redisClient *redis.Client
 
-// func init() {
-// 	redisClient = redis.NewClient(&redis.Options{
-// 		Addr:     "119.23.70.24:25004",
-// 		Password: "",
-// 		DB:       0,
-// 	})
-
-// 	_, err := redisClient.Ping().Result()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
-
-
 func init() {
 	redisClient = redis.NewClient(&redis.Options{
-		Addr:     "127.0.0.1:6379",
+		Addr:     "119.23.70.24:25004",
 		Password: "",
 		DB:       0,
 	})
